3c21725ff9cec46abfb72070e25d83f3b62cae75
Alaamadhoun <alaafm@live.com>
2019-01-03 18:05:47 +0800

    Leaflet Pin Icon Fixed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import L from "leaflet";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+
+var myIcon = L.icon({
+  iconUrl:
+    "https://cdn3.iconfinder.com/data/icons/iconic-1/32/map_pin_fill-512.png",
+  iconSize: [35, 51],
+  iconAnchor: [22, 94],
+  popupAnchor: [-3, -76]
+});
 
 class App extends Component {
   state = {
@@ -18,7 +27,7 @@ class App extends Component {
           attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={position}>
+        <Marker position={position} icon={myIcon}>
           <Popup>
             A pretty CSS3 popup. <br /> Easily customizable.
           </Popup>
